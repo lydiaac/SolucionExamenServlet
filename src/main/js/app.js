6503c9d8c37ce2e3b2b4861b09abee8b09266715
Lydia <lydia.abril@hotmail.com>
2018-02-24 17:15:20 +0100

    Signed-off-by: Lydia <lydia.abril@hotmail.com>

---
+++ src/main/js/app.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+
+import Layout from "./components/Layout"
+
+const app = document.getElementById('app')
+
+ReactDOM.render(
+
+  <Layout />, app);
+
+
